0c67279106199e1c5dc5c2ca523a724388e6c75a
Michael Jacobsen <mj@iqinabox.com>
2022-11-09 09:19:42 +0100

    WIP

--- lua/hello/helloworld.lua
+++ lua/hello/helloworld.lua
@@ -3,9 +3,13 @@ local M = {}
 function M.sayHelloWorld() 
 	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 	if row > 1 then
-		local lines = vim.api.nvim_buf_get_lines(0, row-1, row+1, true)
+		local lines = vim.api.nvim_buf_get_lines(0, row-2, row, true)
+    local t = lines[1]
+    lines[1] =  lines[2]
+    lines[2] = t
+    print(vim.inspect(lines))
+    vim.api.nvim_buf_set_lines(0, row-2, row, true, lines)
 	end
-	print(row, col) 
 end
 
 return M
